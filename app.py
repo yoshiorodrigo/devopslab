2c8caa6b7e675eca2e6ee1a2cb283eb3ec470ca4
yoshiorodrigo <73540489+yoshiorodrigo@users.noreply.github.com>
2023-03-04 16:27:30 -0300

    Bug Removed

--- app.py
+++ app.py
@@ -8,12 +8,3 @@ csrf = CSRFProtect(app)
 @app.route("/")
 def pagina_inicial():
     return "Laboratório Pipeline DevOps"
-
-@app.route('/bug')                                                                                                                                
-def bad():                                                                                                                                        
-    try:                                                                                                                                          
-        raise TypeError()                                                                                                                         
-    except TypeError as e:                                                                                                                        
-        print(e)                                                                                                                                  
-    except TypeError as e:                                                                                                                        
-        print("Duplicado, ou seja, nunca vai entrar aqui.")
